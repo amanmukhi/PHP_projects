7157c2480bd29c14358109dfdebdcdf486b46672
Aman Kumar Mukhi <65829037+amanmukhi@users.noreply.github.com>
2023-01-19 18:56:31 +0530

    Update index.php

--- test/index.php
+++ test/index.php
@@ -5,10 +5,21 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>test-mode</title>
+    <title>test</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 </head>
 
 <body>
+    <h3>How to Disable Right Click using jQuery</h3>
+
+    <script>
+        $(document).ready(function() {
+            $(document).bind("contextmenu", function(e) {
+                // alert('right click disabled');
+                return false;
+            });
+        });
+    </script>
 
 </body>
 
